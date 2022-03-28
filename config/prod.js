334e4e01b0f2cfc7ebcfd443c6894d974838d593
Yoni Lazarus <ylazarus@gmail.com>
2022-03-28 13:05:01 +0300

    first backend commit

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbURL: 'mongodb+srv://ylazarus:1234@cluster0.gym40.mongodb.net/toy_db?retryWrites=true&w=majority'
+  // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+}

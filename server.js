5ed56574861f27705fd80c7bd316a246e95e3f25
Yoni Lazarus <ylazarus@gmail.com>
2022-04-02 22:52:39 +0300

    updated size limit for express.json

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const session = expressSession({
     saveUninitialized: true,
     cookie: { secure: false }
 })
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(session)
 app.use(express.static('public'))
 
